cc5dfcac7a92489aeead82b29d12b3c719cad7f2
Lekapp <contacto@lekapp.cl>
2025-03-27 10:44:31 -0300

    .

--- index.php
+++ index.php
@@ -78,7 +78,7 @@ if (isset($_ENV['CI_ENVIRONMENT'])) {
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
 
 /*
  *---------------------------------------------------------------
