6bd6ff1f125b6b2f0f3e105a597ff88561f0f545
Francisco Barrera <109861603+genkaistudiodev@users.noreply.github.com>
2025-06-09 21:19:41 -0400

    power bi integrado

--- application/views/special/dashboard_content.php
+++ application/views/special/dashboard_content.php
@@ -4,12 +4,12 @@
 		<div class="row">
 			<div class="col-lg-12">
 				<div class="card">
-					<div class="card-header d-flex align-items-center">           
+					<div class="card-header d-flex align-items-center">
 						<h2 class="h3">Escritorio </h2>
 					</div>
 					<div class="card-body">
-						
-						
+
+						<iframe title="dashboard" width="100%" height="600" src="https://app.powerbi.com/reportEmbed?reportId=a2852942-3a51-48c3-82f1-1a33bc18eadb&autoAuth=true&ctid=e7df0fa8-4ebd-4888-915a-ae3494dca78f" frameborder="0" allowFullScreen="true"></iframe>
 
 					</div>
 				</div>
@@ -19,29 +19,29 @@
 </section>
 
 <script type="text/javascript">
-	$(document).ready(function(){
+	$(document).ready(function() {
 		$('#data').DataTable({
 			"language": {
-				"sProcessing":     "Procesando...",
-				"sLengthMenu":     "Mostrar _MENU_ registros",
-				"sZeroRecords":    "No se encontraron resultados",
-				"sEmptyTable":     "Ningún dato disponible en esta tabla",
-				"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-				"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-				"sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-				"sInfoPostFix":    "",
-				"sSearch":         "Buscar:",
-				"sUrl":            "",
-				"sInfoThousands":  ",",
+				"sProcessing": "Procesando...",
+				"sLengthMenu": "Mostrar _MENU_ registros",
+				"sZeroRecords": "No se encontraron resultados",
+				"sEmptyTable": "Ningún dato disponible en esta tabla",
+				"sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+				"sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+				"sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+				"sInfoPostFix": "",
+				"sSearch": "Buscar:",
+				"sUrl": "",
+				"sInfoThousands": ",",
 				"sLoadingRecords": "Cargando...",
 				"oPaginate": {
-					"sFirst":    "Primero",
-					"sLast":     "Último",
-					"sNext":     "Siguiente",
+					"sFirst": "Primero",
+					"sLast": "Último",
+					"sNext": "Siguiente",
 					"sPrevious": "Anterior"
 				},
 				"oAria": {
-					"sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+					"sSortAscending": ": Activar para ordenar la columna de manera ascendente",
 					"sSortDescending": ": Activar para ordenar la columna de manera descendente"
 				}
 			}
